633e439fd96d1242a9142c05aecdbf00b2e6968a
EC2 Default User <ec2-user@ip-172-31-11-158.us-east-2.compute.internal>
2017-10-25 22:12:19 +0000

    bug fixes and modications to experiment set up

--- time_interval_testing.py
+++ time_interval_testing.py
@@ -49,15 +49,31 @@ aws_host = config.get("AWSDatabaseConfig", "host")
 ###############################################################################
 
 # Insert keywords to search as a list of strings
-keywords = [
-    'vladimir putin',
+#keywords = [
+#    'vladimir putin',
     # 'nba',
     # 'donald trump'
-]
+#]
+keywords = []
+db = pymysql.connect(user= aws_username,  # Username of AWS database
+                                 passwd= aws_password,  # AWS Database password
+                                 host= aws_host,  # AWS Instance
+                                 port=3306,
+                                 database='google_time_test',
+                                 charset='utf8mb4')
+cur = db.cursor()
+cur.execute("SELECT * FROM google_time_test.keyterms WHERE active = 1 order by term_id")
+keyterms_db = cur.fetchall()
+for item in keyterms_db:
+    keywords.append(item[2])
+
+cur.close()
+
 
 # See in the config.cfg file for possible values
 config = {
-    'use_own_ip': True,
+    'use_own_ip': False,
+    'proxy_file': 'proxies.txt',
     'keywords': keywords,
     'search_engines': ['google',],
     'num_pages_for_keyword': 1,
@@ -68,22 +84,23 @@ config = {
     'search_type': 'normal',
     'clean_cache_files': False,
     'print_results': 'summarize',
+    'detection_prevention_sleep': True, # tells the scraper not to wait to scrape b/c time precision important for time interval testing
 
 }
 
 # How many times you want to collect each time interval
-repeat_num = 3
+repeat_num = 1440 #1440
 counter = 0
+test_run_id = 31
 
 # The timer intervals, currently in seconds
-timer_interval = [120, 200, 300]
+#timer_interval = [60, 120, 240, 480, 960, 1920, 3840]
+timer_interval = [60]
 
-# This is the interval the first set of repeats will run on. This number should
-# not be included in the timer_interval list.
-current_interval = 60
+# This is the interval the first set of repeats will run on. 
+current_interval = timer_interval[0]
 
-def put_in_database():
-
+def collect_data():
 
     try:
         os.remove("google_scraper.db")
@@ -109,7 +126,7 @@ def put_in_database():
                                  passwd= aws_password,  # AWS Database password
                                  host= aws_host,  # AWS Instance
                                  port=3306,
-                                 database='google_scraper',
+                                 database='google_time_test',
                                  charset='utf8mb4')
     cur = db.cursor()
 
@@ -118,29 +135,27 @@ def put_in_database():
         for serp in search.serps:
 
             # Create a new record into serp table
-            sql = "INSERT INTO serp_topstories_TESTING (search_engine_name, scrape_method, requested_at, search_query) VALUES (%s, %s, %s, %s)"
-            cur.execute(sql,(serp.search_engine_name, serp.scrape_method, serp.requested_at, serp.query))
+            sql = "INSERT INTO serp (search_engine_name, scrape_method, requested_at, search_query, test_run_id) VALUES (%s, %s, %s, %s, %s)"
+            cur.execute(sql,(serp.search_engine_name, serp.scrape_method, serp.requested_at, serp.query, test_run_id))
 
             serp_id = cur.lastrowid  # This line can go before or after db.commit()
-            print("\nserp_id is:  ", serp_id)
+            #print("\nserp_id is:  ", serp_id)
 
             db.commit()
 
 
             for link in serp.links:
-
+                #print (link.link_type)
                 if link.link_type == "top_stories":
 
                     if link.has_image:
                         link.has_image = 1
                     else: link.has_image = 0
 
-                    print("\nCurrent Interval is:  ", current_interval)
-                    get_datetime = datetime.datetime.now()
-                    print(datetime.datetime.now())
+                    result_datetime = datetime.datetime.now()
                     # Create a new record into search_engine_results table
-                    sql = "INSERT INTO search_engine_topstories_TESTING (link, title, rank, link_type, serp_id, has_image, image_dims, news_date, news_source, timer_interval, get_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                    cur.execute(sql,(link.link, link.title, link.rank, link.link_type, serp_id, link.has_image, link.image_dims, link.news_date, link.news_source, current_interval, get_datetime))
+                    sql = "INSERT INTO search_results (serp_id, link, title, rank, link_type, has_image, image_dims, news_date, news_source, timer_interval, result_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    cur.execute(sql,(serp_id, link.link, link.title, link.rank, link.link_type, link.has_image, link.image_dims, link.news_date, link.news_source, current_interval, result_datetime))
 
                     db.commit()
 
@@ -153,14 +168,15 @@ def put_in_database():
 
         # enter this section once completed desired number of repetitions as denoted by repeat_num
         # if there are remaining intervals left, do:
-        if timer_interval:
+        timer_interval.pop(0)
+        if len(timer_interval) > 0:
 
             counter = 0
-
+            
             print("\nThe next interval time is: ", timer_interval[0])
             current_interval = timer_interval[0]
-            scheduler.reschedule_job('job_1', trigger='interval', seconds=timer_interval[0])
-            timer_interval.pop(0)
+            scheduler.reschedule_job('job_1', trigger='interval', seconds=current_interval)
+            
 
         # if there are no more intervals left, we're done:
         else:
@@ -172,15 +188,15 @@ def put_in_database():
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(put_in_database, 'interval', seconds=current_interval, id='job_1', replace_existing=True)
+    scheduler.add_job(collect_data, 'interval', seconds=current_interval, id='job_1', replace_existing=True)
     scheduler.start()
 
     try:
         # This is here to simulate application activity (which keeps the main thread alive).
         while True:
-            time.sleep(2)
+            time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         # Not strictly necessary if daemonic mode is enabled but should be done if possible
         scheduler.shutdown()
 
-# nohup python -u top_stories_time_testing.py > nohup_intervals.txt &
+# nohup python -u time_interval_testing.py > nohup_intervals.txt &
